0a9a5e79e4c024f99a130deda6bc5071a4a2febe
dheeraj kumar <dheerajkumar@LP-C02WC0H1G8WL.local>
2020-06-18 23:04:39 -0400

    adding new line

--- terraform-plans/k8.tf
+++ terraform-plans/k8.tf
@@ -3,67 +3,67 @@ resource "azurerm_resource_group" "k8s" {
     location = var.location
 }
 
-resource "random_id" "log_analytics_workspace_name_suffix" {
-    byte_length = 8
-}
+# resource "random_id" "log_analytics_workspace_name_suffix" {
+#     byte_length = 8
+# }
 
-resource "azurerm_log_analytics_workspace" "test" {
-    # The WorkSpace name has to be unique across the whole of azure, not just the current subscription/tenant.
-    name                = "${var.log_analytics_workspace_name}-${random_id.log_analytics_workspace_name_suffix.dec}"
-    location            = var.log_analytics_workspace_location
-    resource_group_name = azurerm_resource_group.k8s.name
-    sku                 = var.log_analytics_workspace_sku
-}
+# resource "azurerm_log_analytics_workspace" "test" {
+#     # The WorkSpace name has to be unique across the whole of azure, not just the current subscription/tenant.
+#     name                = "${var.log_analytics_workspace_name}-${random_id.log_analytics_workspace_name_suffix.dec}"
+#     location            = var.log_analytics_workspace_location
+#     resource_group_name = azurerm_resource_group.k8s.name
+#     sku                 = var.log_analytics_workspace_sku
+# }
 
-resource "azurerm_log_analytics_solution" "test" {
-    solution_name         = "ContainerInsights"
-    location              = azurerm_log_analytics_workspace.test.location
-    resource_group_name   = azurerm_resource_group.k8s.name
-    workspace_resource_id = azurerm_log_analytics_workspace.test.id
-    workspace_name        = azurerm_log_analytics_workspace.test.name
+# resource "azurerm_log_analytics_solution" "test" {
+#     solution_name         = "ContainerInsights"
+#     location              = azurerm_log_analytics_workspace.test.location
+#     resource_group_name   = azurerm_resource_group.k8s.name
+#     workspace_resource_id = azurerm_log_analytics_workspace.test.id
+#     workspace_name        = azurerm_log_analytics_workspace.test.name
 
-    plan {
-        publisher = "Microsoft"
-        product   = "OMSGallery/ContainerInsights"
-    }
-}
+#     plan {
+#         publisher = "Microsoft"
+#         product   = "OMSGallery/ContainerInsights"
+#     }
+# }
 
-resource "azurerm_kubernetes_cluster" "enterprise" {
-    name                = var.cluster_name
-    location            = azurerm_resource_group.k8s.location
-    resource_group_name = azurerm_resource_group.k8s.name
-    dns_prefix          = var.dns_prefix
+# resource "azurerm_kubernetes_cluster" "enterprise" {
+#     name                = var.cluster_name
+#     location            = azurerm_resource_group.k8s.location
+#     resource_group_name = azurerm_resource_group.k8s.name
+#     dns_prefix          = var.dns_prefix
 
-    linux_profile {
-        admin_username = "ubuntu"
+#     linux_profile {
+#         admin_username = "ubuntu"
 
-        ssh_key {
-            key_data = file(var.ssh_public_key)
-        }
-    }
+#         ssh_key {
+#             key_data = file(var.ssh_public_key)
+#         }
+#     }
 
-    default_node_pool {
-        name            = "agentpool"
-        node_count      = var.agent_count
-        vm_size         = "Standard_DS2_v2"
-    }
+#     default_node_pool {
+#         name            = "agentpool"
+#         node_count      = var.agent_count
+#         vm_size         = "Standard_DS2_v2"
+#     }
 
-    service_principal {
-        client_id     = var.client_id
-        client_secret = var.client_secret
-    }
+#     service_principal {
+#         client_id     = var.client_id
+#         client_secret = var.client_secret
+#     }
 
-    addon_profile {
-        oms_agent {
-        enabled                    = true
-        log_analytics_workspace_id = azurerm_log_analytics_workspace.test.id
-        }
-    }
+#     addon_profile {
+#         oms_agent {
+#         enabled                    = true
+#         log_analytics_workspace_id = azurerm_log_analytics_workspace.test.id
+#         }
+#     }
 
-    tags = {
-        Environment = "Development"
-    }
-}
+#     tags = {
+#         Environment = "Development"
+#     }
+# }
 
 resource "azurerm_container_registry" "acr" {
   name                     = "containerRegistryEnterprise"
