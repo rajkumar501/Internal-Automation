dcc53481589604002e840431495b840671fefc09
dheerajyerram <dheerajyerram@gmail.com>
2020-07-21 23:03:04 -0400

    update

--- terraform-plans/node-pools.tf
+++ terraform-plans/node-pools.tf
@@ -3,10 +3,10 @@
 resource "azurerm_kubernetes_cluster_node_pool" "firstpool" {
   name                  = "internal"
   kubernetes_cluster_id = azurerm_kubernetes_cluster.enterprise.id
-  vm_size               = "Standard_DS1_v2"
+  vm_size               = "Standard_DS2_v2"
   enable_auto_scaling  = "true"
-  min_count = "1"
-  max_count = "1"
+  min_count = "2"
+  max_count = "5"
   tags = {
     Environment = "dev"
   }
