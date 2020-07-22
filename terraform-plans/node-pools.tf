78ccefb019e9baeded9984ff697ffdaaa94ecd52
dheerajyerram <dheerajyerram@gmail.com>
2020-07-21 20:37:05 -0400

    update

--- terraform-plans/node-pools.tf
+++ terraform-plans/node-pools.tf
@@ -1,10 +1,7 @@
 
-data "azurerm_subnet" "aks_subnet"{
-}
 
 resource "azurerm_kubernetes_cluster_node_pool" "firstpool" {
   name                  = "internal"
-  kubernetes_cluster_id = azurerm_kubernetes_cluster.enterprise.id
   vm_size               = "Standard_DS1_v2"
   enable_auto_scaling  = "true"
   min_count = "2"
