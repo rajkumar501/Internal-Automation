9c85ead53fdfd8d7294342bc17d7f667118d8155
dheerajyerram <dheerajyerram@gmail.com>
2020-07-22 09:30:55 -0400

    update

--- terraform-plans/node-pools.tf
+++ terraform-plans/node-pools.tf
@@ -3,10 +3,11 @@
 resource "azurerm_kubernetes_cluster_node_pool" "apppool" {
   name                  = "app"
   kubernetes_cluster_id = azurerm_kubernetes_cluster.enterprise.id
-  vm_size               = "Standard_DS1_v2"
-  enable_auto_scaling  = "true"
-  min_count = "2"
-  max_count = "5"
+  vm_size               = "Standard_DS2_v2"
+  //enable_auto_scaling  = "true"
+ // min_count = "2"
+  // max_count = "5"
+  node_count= "3"
   tags = {
     Environment = "dev"
   }
